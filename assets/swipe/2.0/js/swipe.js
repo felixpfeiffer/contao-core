ab6e15e985b235b1db5ee2a39009724a2b10b605
Leo Feyer <github@contao.org>
2014-03-24 18:26:53 +0100

    Fix the Swipe "continuous" option if there are exactly two slides (see #6812)

--- assets/swipe/2.0/js/swipe.js
+++ assets/swipe/2.0/js/swipe.js
@@ -131,7 +131,7 @@ function Swipe(container, options) {
         // set the new menu reference, so we have it in updateMenu()
         options.menu = childs[h];
 
-        for (var i=0; i<slides.length; i++) {
+        for (var i=0; i<length; i++) {
 
           var b = document.createElement('b');
           b.innerHTML = '•';
@@ -156,7 +156,7 @@ function Swipe(container, options) {
 
   function updateMenu() {
 
-    for (var i=0; i<slides.length; i++) {
+    for (var i=0; i<length; i++) {
 
       var child = options.menu.children[i];
       child.className = (parseInt(child.getAttribute ? child.getAttribute('data-index') : child.attributes['data-index'].nodeValue) == index) ? 'active' : '';
